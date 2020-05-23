a91deffb0b710f196c82688cdfb57279ebf729c6
Grejo Joby <grejo00@gmail.com>
2020-05-23 20:27:33 +0530

    Dark Mode

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import { Cards, Chart, CountryPicker } from "./component";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
 
+import coronaImage from './images/image.png';
+
 class App extends React.Component {
   state = {
     data: {},
+    country: '',
   };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async(country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+
+  }
   render() {
-    const { data } = this.state;
+    const { data,country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="COVID-19"/>
         <Cards data={data} />
-        <Chart />
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
+        <Chart data={data} country={country} />
+        
       </div>
     );
   }
--- src/component/Chart/Chart.jsx
+++ src/component/Chart/Chart.jsx
@@ -4,15 +4,15 @@ import {Line,Bar} from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart = () => {
-    const [ dailyData,setDailyData] = useState([]);
+const Chart = ({data: { confirmed, recovered, deaths }, country}) => {
+    const [dailyData,setDailyData] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
         }
         fetchAPI();
-    });
+    },[]);
 
     const lineChart = (
         dailyData.length
@@ -37,9 +37,29 @@ const Chart = () => {
         /> ): null
 
     );
+    // console.log(confirmed);
+    const barChart = (
+        confirmed
+        ? (
+            <Bar
+                data={{
+                    labels: ['Infected', 'Recovered','Deaths'],
+                    datasets: [{
+                        label: 'People',
+                        backgroundColor: ['rgba(0,0,255,0.5)','rgba(0,255,0,0.5)','rgba(255,0,0,0.5)'],                       
+                        data:[confirmed.value,recovered.value,deaths.value],
+                }]
+                }}
+                options={{
+                    legend: {display : false},
+                    title: {display : true, text: `Current state in ${country}`},
+                }}
+            />
+        ) : null
+    );
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     )
 }
